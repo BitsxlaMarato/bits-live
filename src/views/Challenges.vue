43f31965caeec6a6887a6fb904e291e2eb056116
Carlota Catot Bragós <carkbra@gmail.com>
2022-12-12 22:55:45 +0100

    added more challenges

--- src/views/Challenges.vue
+++ src/views/Challenges.vue
@@ -2,25 +2,10 @@
   <div id="schedule" class="under-header padding-bottom">
     <div class="container">
       <div class="panel large">
-        <h1><a href="https://www.fib.upc.edu/ca/la-marato/" target="_blank" class="external-link" style="color: #dddddd">Descripcions completes dels projectes</a></h1>
-      </div>
-      <div class="panel">
         <h1>El ratolí com a eina per lluitar contra l'assassí d'Albert Einstein</h1>
         <div class="text-centered">
           <p class="icon"><i class="fab fa-slack" aria-hidden="true"></i></p>
           <span>albert-einstein</span>
-        </div>
-        <div>
-          <div class="embed-container">
-            <iframe src="https://www.youtube.com/embed/vZWxzslBvx8" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-          </div>
-        </div>
-      </div>
-      <div class="panel">
-        <h1>Eina de Suport per la Planificació de les Tasques Acadèmiques</h1>
-        <div class="text-centered">
-          <p class="icon"><i class="fab fa-slack" aria-hidden="true"></i></p>
-          <span>planificacio-tasques</span>
         </div>
         <div>
           <p class="margin-bottom">
@@ -40,31 +25,95 @@
           </div>
           <br>
           <div class="margin-bottom text-centered">
-            <b>Proposed by:</b> <span>BSC</span>
+            <b>Proposen i col·laboren:</b> <span>CIBER</span> <span>CSIC</span> <span>Institut de Recerca de l'Hospital de la Santa Creu i Sant Pau</span> <span>Banc de Sang i Teixits</span>
           </div>
         </div>
       </div>
-      <div class="panel">
-        <h1>Interacció de proteïnes. Ens fiquem d’acord?</h1>
+      <div class="panel large">
+        <h1>Tenir un cor que no et cap al pit no sempre és bo... el tens?!</h1>
         <div class="text-centered">
           <p class="icon"><i class="fab fa-slack" aria-hidden="true"></i></p>
-          <span>proteines</span>
+          <span>cor-al-pit</span>
         </div>
         <div>
-          <div class="embed-container">
-            <iframe src="https://www.youtube.com/embed/STDggsIY-OM" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <p class="margin-bottom">
+            La insuficiència cardíaca és la nova pandèmia del segle XXI. En aquesta malaltia el cor no bombeja adequadament la sang, situació que impacta molt negativament en la qualitat de vida d'aquests pacients. La insuficiència cardíaca, que arriba a afectar 26 milions de persones a nivell mundial, pot posar en perill la vida de les persones que la pateixen. En els casos més greus l'única solució per aquests pacients és el trasplantament cardíac. Malgrat els avenços significatius en el tractament i la prevenció, la prevalença d'aquesta malaltia va en augment, la mortalitat i morbiditat segueixen sent elevades, i la qualitat de vida dels pacients molt deficient. És per tant imprescindible identificar noves estratègies de tractament en aquests pacients i això requereix estudis en animals d'experimentació amb tecnologia puntera.
+          </p>
+          <div>
+            <h3 class="margin-bottom">Punt de vista tecnològic</h3>
+            <p>
+              L'ecografia cardíaca és l'eina utilitzada per diagnosticar la hipertrofia i la disfunció cardíaques. En aquest repte busquem disposar d'una aplicació que ens permeti analitzar els paràmetres ecocardiogràfics referents a la morfologia i la funció del cor de manera objectiva i automatitzada i que ens alerti quan es detecti la presència d'anomalies  en algun dels paràmetres habituals. Et veus amb cor de fer-ho? Et necessitem!
+            </p>
+          </div>
+          <div class="margin-bottom">
+            <h3 class="margin-bottom">Punt de vista de la salut</h3>
+            <p>
+              El funcionament correcte del cor és de vital importància ja que s’encarrega de distribuir la sang a la resta d’òrgans. La hipertensió, l'infart de miocardi i les malalties valvulars entre altres, provoquen una resposta adaptativa del cor que modifica la seva morfologia, remodelant el gruix de les seves parets i disminuint el volum de les seves cavitats. Així, l’habilitat de bombejar correctament la sang a l’organisme es veu reduïda i s’esdevé la insuficiència cardíaca. Aquesta patologia suposa un greu problema de salut en la societat occidental amb un impacte econòmic molt important. Es preveu que tan sols a Estats Units aquesta malaltia pugui arribar a afectar a 8 milions de persones l'any 2030.  Els tractaments actuals són poc efectius per prevenir o frenar el desenvolupament de la malaltia, i fonamentalment van dirigits a millorar la qualitat de vida. Per això, és de gran importància identificar noves dianes terapèutiques en aquesta malaltia i per això és necessari realitzar estudis en models animals.
+            </p>
+          </div>
+          <br>
+          <div class="margin-bottom text-centered">
+            <b>Proposen i col·laboren:</b> <span>CIBER</span> <span>CSIC</span> <span>Institut de Recerca de l'Hospital de la Santa Creu i Sant Pau</span> <span>Banc de Sang i Teixits</span>
           </div>
         </div>
       </div>
-      <div class="panel">
-        <h1>M’estimo i m’animo</h1>
+      <div class="panel large">
+        <h1>Alerta Trombosi: perquè la vida corre per les teves venes</h1>
         <div class="text-centered">
           <p class="icon"><i class="fab fa-slack" aria-hidden="true"></i></p>
-          <span>estimo-animo</span>
+          <span>alerta-trombo</span>
         </div>
         <div>
-          <div class="embed-container">
-            <iframe src="https://www.youtube.com/embed/Sq0a3JdJ9Fo" class="embed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+          <p class="margin-bottom">
+            Crear una solució que permeti a la població en general identificar simptomatologia previ a l'ocurrència d'esdeveniment trombòtic i, que expliqui i conscienciï lúdicament sobre la trombosi. Per exemple, es podria crear un videojoc educatiu que permeti entendre el procés de trombosi (amb trombòcits com a protagonistes!!) i que doni com a resultat un procés d'enteniment i prevenció als seus jugadors. La idea seria publicar el joc en una plataforma de codi obert, però permetre a l'Associació ActivaTT posar-ho en una AppStore perquè els usuaris li ho descarreguessin a un preu simbòlic-solidari (e.g. 0.99 €) i que els fons es dediquin a l'Associació.
+          </p>
+          <div>
+            <h3 class="margin-bottom">Punt de vista tecnològic</h3>
+            <p>
+              Un videojoc/app que integrés aspectes de la fisiologia de la trombosi, què és, que símptomes té, com prevenir-la podria tenir un alt impacte en la conscienciació de la importància d'aquesta patologia i en la prevenció. Com fer-ho? Està totalment obert a la vostra creativitat, des de Xartec Salut i l'associació ActivaTT, us poden donar informació de les idees i aspectes que, o hauria de presentar el joc, app que penseu que pot ajudar. Heu de pensar en una forma lúdica i possiblement educativa que mostri fàcilment els possibles símptomes de trombosi.
+            </p>
+          </div>
+          <div class="margin-bottom">
+            <h3 class="margin-bottom">Punt de vista de la salut</h3>
+            <p>
+              La trombosi venosa profunda (TVP) és una afecció que ocorre quan es forma un coàgul de sang en una vena profunda. Aquests coàguls en general es formen en la part inferior de les cames, les cuixes o la pelvis, però també poden aparèixer al braç. És important saber el que és la TVP perquè pot afectar qualsevol persona i causar-li una malaltia greu, una discapacitat i, en alguns casos, la mort. La bona notícia és que es pot prevenir.<br>
+              La complicació més greu de la TVP ocorre quan una part del coàgul es desprèn i viatja pel torrent sanguini fins als pulmons, causant una embòlia pulmonar (EP). Aquest coàgul pot impedir que la sang arribi als pulmons i és mortal.<br>
+              Les xifres associades a la trombosi són alarmants ja que afecta anualment 1-2 persones per 1.000 habitants, amb una mortalitat del 12,6% dels pacients en el primer any. Dels supervivents, un 25-50% desenvolupa molèsties cròniques de les cames, a causa del mal que ocasiona el coàgul en les vàlvules de la vena, que redueixen severament la mobilitat (síndrome posttrombòtica). El risc de trombosi augmenta considerablement amb l'edat, arribant a l'1 per 100 individus/any en persones majors de 65 anys. La trombosi és la principal causa prevenible de mortalitat en pacients hospitalitzats per qualsevol altra patologia i la primera causa de mort en pacients oncològics tractats amb quimioteràpia i en dones embarassades.<br>
+              La trombosi té tractament, la teràpia anticoagulant, però dels pacients que han sofert una trombosi, fins al 6% per any sofriran un nou esdeveniment trombòtic (trombosi recurrent) una vegada finalitzat el tractament.<br>
+              Les últimes dades revelen que només a Europa es produeixen al voltant de 1,5 milions de trombosis a l'any, produint més de 543.000 morts (el que suposa 1 mort per trombosi cada minut). La trombosi genera una despesa directa, només a Europa, estimat en 3 mil milions d'euros a l'any.
+            </p>
+          </div>
+          <br>
+          <div class="margin-bottom text-centered">
+            <b>Proposen i col·laboren:</b> <span>Xartec Salut</span> <span>Associació ActivaTT</span>
+          </div>
+        </div>
+      </div>
+      <div class="panel large">
+        <h1>Computació entre cloud i edge per a models de risc de Ictus basats en senyals biomèdics de dispositius portàtils</h1>
+        <div class="text-centered">
+          <p class="icon"><i class="fab fa-slack" aria-hidden="true"></i></p>
+          <span>ictus</span>
+        </div>
+        <div>
+          <p class="margin-bottom">
+            El desafiament s'enfoca a desenvolupar una aplicació per a telèfons mòbils que sigui capaç de capturar i processar senyals biomèdics des d'un dispositiu portàtil. Aquesta informació s'usa per a alimentar models d'AI/ML per a l'avaluació del risc d'accident cerebrovascular que resideixen en un clúster amb capacitat d'inferència sota demanda. El resultat esperat és la simulació d'un escenari realista en el qual es produeixen prediccions de risc d'accident cerebrovascular basades en dades de sensors en el continu entre edge i cloud.
+          </p>
+          <div>
+            <h3 class="margin-bottom">Punt de vista tecnològic</h3>
+            <p>
+              A mesura que els dispositius es tornen més petits i portàtils, el poder del núvol ha d'anar acompanyat d'estratègies per a augmentar el rendiment i millorar la privacitat en el edge. Diverses startups en el mercat estan avançant en intel·ligència distribuïda amb el disseny de sistemes IoT per a la cura de la salut. En particular, els dispositius portàtils i els sensors biomètrics representen les noves fronteres de la computació distribuïda amb integració d'IA i aprenentatge automàtic que brinden retroalimentació en temps real que permet la florida actual del mercat de tecnologies de self-monitoring (rellotges intel·ligents i altres perifèrics, aplicacions de programari mòbil, etc.).
+            </p>
+          </div>
+          <div class="margin-bottom">
+            <h3 class="margin-bottom">Punt de vista de la salut</h3>
+            <p>
+              L'accident cerebrovascular o ictus és una afecció neurovascular deguda a una lesió focal aguda en el sistema nerviós central per una causa vascular. És una malaltia comuna, amb una incidència creixent en l'envelliment, que afecta una de cada quatre persones al llarg de la seva vida. És la segona causa principal de mort i la tercera causa principal de discapacitat en adults a tot el món. S'han desenvolupat noves estratègies per a prevenir i monitorar el ictus o la seva recurrència, de manera contínua i no invasiva, concretament l'ús de dispositius portàtils i mòbils intel·ligents.
+            </p>
+          </div>
+          <br>
+          <div class="margin-bottom text-centered">
+            <b>Proposen i col·laboren:</b> <span>BSC-CNS</span> <span>AI Sprint</span>
           </div>
         </div>
       </div>
