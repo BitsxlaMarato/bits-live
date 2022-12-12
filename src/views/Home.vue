d9a2be3d01bdc0130ec03fa2ed19033cef06d60e
Carlota Catot Bragós <carkbra@gmail.com>
2022-12-12 23:05:06 +0100

    changed home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,39 +5,20 @@
         <h1>Informació important</h1>
         <h3 class="text-centered margin-bottom">Credencials pel Wi-Fi</h3>
         <div class="flex-containers">
-          <div class="width-45-resp">
-            <div class="margin-bottom text-centered">
-              <p class="icon"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></p>
-              <span>Barcelona</span>
-            </div>
-            <div>
-              <div class="panel-info text-centered" style="font-size: 13px">
-                <p class="label text-normal"><b>Xarxa:</b> BxMsM</p><br>
-                <p class="label text-normal"><b>Contrasenya:</b> BxMSM21$</p>
-              </div>
-            </div>
+          <div class="margin-bottom text-centered">
+            <p class="icon"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></p>
+            <span>Barcelona</span>
           </div>
-          <div class="width-45-resp">
-            <div class="margin-bottom text-centered">
-              <p class="icon"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></p>
-              <span>Lleida</span>
-            </div>
-            <div>
-              <div class="panel-info text-centered" style="font-size: 13px">
-                <p class="label text-normal"><b>Xarxa:</b> congres</p><br>
-                <p class="label text-normal"><b>Usuari:</b> #cwcapppont14</p><br>
-                <p class="label text-normal"><b>Contrasenya:</b> Ma@lX2210!</p>
-              </div>
+          <div>
+            <div class="panel-info text-centered" style="font-size: 13px">
+              <p class="label text-normal"><b>Xarxa:</b> #BITSxLAMARATO </p><br>
+              <p class="label text-normal"><b>Contrasenya:</b> 400?BxM22 </p>
             </div>
           </div>
         </div>
         <div>
           <div>
             <h3 class="text-centered margin-bottom" style="margin-top: 0">Links</h3>
-          </div>
-          <div class="buttons margin-bottom">
-            <a target="_blank" href="https://www.twitch.tv/hackersupc" class="external-link"><i class="fab fa-twitch" aria-hidden="true"></i> Xerrades al twitch de Hackers@UPC</a>
-            <a target="_blank" href="https://www.twitch.tv/hackersupc" class="external-link"><i class="fab fa-twitch" aria-hidden="true"></i> Xerrades al twitch de LleidaHack</a>
           </div>
           <div class="buttons margin-bottom">
             <a target="_blank" href="https://bitsxlamarato-2021.devpost.com/" class="external-link">Devpost</a>
@@ -68,16 +49,6 @@
           </div>
         </div>
         <div>
-          <h3 class="text-centered margin-bottom">Xarxes socials de LleidaHack</h3>
-          <div class="buttons">
-            <a class="tw" target="_blank" href="https://twitter.com/LleidaHack"><i class="fab fa-twitter" aria-hidden="true"></i></a>
-            <a class="ig" target="_blank" href="https://www.instagram.com/lleidahack"><i class="fab fa-instagram" aria-hidden="true"></i></a>
-            <a class="yt" target="_blank" href="https://www.youtube.com/c/LleidaHack"><i class="fab fa-youtube" aria-hidden="true"></i></a>
-            <a class="yt" target="_blank" href="https://www.twitch.tv/lleidahack"><i class="fab fa-twitch" aria-hidden="true"></i></a>
-            <a class="gt" target="_blank" href="https://www.linkedin.com/company/lleidahack/"><i class="fab fa-linkedin" aria-hidden="true"></i></a>
-          </div>
-        </div>
-        <div>
           <h3 class="text-centered margin-bottom">Xarxes socials de FIB</h3>
           <div class="buttons">
             <a class="fb" target="_blank" href="https://www.facebook.com/fib.upc"><i class="fab fa-facebook" aria-hidden="true"></i></a>
@@ -92,71 +63,28 @@
         <div class="get-there">
           <h2 class="get-title text-centered margin-bottom"><i class="fa fa-train" aria-hidden="true"></i> Amb tren</h2>
           <div class="flex-containers">
-            <div class="width-45-resp margin-bottom-first">
-              <div class="margin-bottom text-centered">
-                <p class="icon"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></p>
-                <span>Barcelona</span>
-              </div>
+            <div class="margin-bottom text-centered">
               <p>Si veniu amb tren, heu d'agafar l'L3, direcció <b>Zona Universitària</b> fins a l'estació de <b>Palau Reial</b>. <br><br> Des de palau reial heu de pujar cap al Campus Nord, situat al damunt de la facultat d'economia de la UB. També podeu agafar el tram fins a la parada de <b>Palau Reial</b>.</p>
             </div>
-            <div class="width-45-resp">
-              <div class="margin-bottom text-centered">
-                <p class="icon"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></p>
-                <span>Lleida</span>
-              </div>
-                <p>L'estació de Lleida ciutat és <b>Lleida-Pirineus</b>. <br><br> Des d'allí, agafar recta la Rambla Ferran, paral·lela al riu, fins a arribar al pont de la universitat (davant de l'estació de busos), creuar el pont. Un cop creuat el pont a mà esquerra, el segon edifici (Escola Politècnica Superior), portal número 69.</p>
-            </div>
-          </div>
         </div>
         <div class="get-there">
           <h2 class="get-title text-centered margin-bottom"><i class="fa fa-bus" aria-hidden="true"></i> Amb autobús</h2>
           <div class="flex-containers">
-            <div class="width-45-resp margin-bottom-first">
-              <div class="margin-bottom text-centered">
-                <p class="icon"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></p>
-                <span>Barcelona</span>
-              </div>
-              <p>Si veniu amb autobús, us serviran qualsevol dels següents autobusos:</p>
-              <ul>
-                <li><b>33</b> fins a la parada <b>Jordi Girona - John M Keynes</b>  un cop allà estareu al costat del campus.</li>
-                <li><b>7</b>, <b>67</b>, <b>75</b>, <b>113</b> o <b>H6</b>  fins a la parada <b>Palau Reial</b>, des de palau reial heu de pujar cap al Campus Nord, situat al damunt de la facultat d'economia de la UB.</li>
-              </ul>
-            </div>
-            <div class="width-45-resp">
-              <div class="margin-bottom text-centered">
-                <p class="icon"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></p>
-                <span>Lleida</span>
-              </div>
-              <p>Si veniu amb autobús, heu de seguir les instruccions depenent del vostre origen:</p>
-              <ul>
-                <li>En cas de venir de <b>fora de Lleida</b>, la parada més propera és la mateixa <b>estació de busos</b>. Creuar el pont. Un cop creuat el pont a mà esquerra, el segon edifici (Escola Politècnica Superior), portal número 69.</li>
-                <li>En cas d'<b>estar a Lleida</b>, la parada més propera es <b>Pont Universitat</b></li>
-              </ul>
-            </div>
+            <p>Si veniu amb autobús, us serviran qualsevol dels següents autobusos:</p>
+            <ul>
+              <li><b>33</b> fins a la parada <b>Jordi Girona - John M Keynes</b>  un cop allà estareu al costat del campus.</li>
+              <li><b>7</b>, <b>67</b>, <b>75</b>, <b>113</b> o <b>H6</b>  fins a la parada <b>Palau Reial</b>, des de palau reial heu de pujar cap al Campus Nord, situat al damunt de la facultat d'economia de la UB.</li>
+            </ul>
           </div>
         </div>
         <div class="get-there">
           <h2 class="get-title text-centered margin-bottom"><i class="fa fa-car" aria-hidden="true"></i> Amb cotxe</h2>
           <div class="flex-containers">
-            <div class="width-45-resp">
-              <div class="margin-bottom text-centered margin-bottom-first">
-                <p class="icon"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></p>
-                <span>Barcelona</span>
-              </div>
-              <p>Si veniu amb cotxe, podreu trobar lloc on aparcar a la part superior del campus, pels carrers: <i>Sor Eulàlia d'Anzizu</i> o <i>Cavallers</i>.</p>
-            </div>
-            <div class="width-45-resp">
-              <div class="margin-bottom text-centered">
-                <p class="icon"><i class="fas fa-map-marker-alt" aria-hidden="true"></i></p>
-                <span>Lleida</span>
-              </div>
-              <p>Si veniu amb cotxe, podreu trobar lloc just davant de la EPS, normalment. Si no, just al darrere del campus hi ha un pàrquing públic gratuït.</p>
-            </div>
+            <p>Si veniu amb cotxe, podreu trobar lloc on aparcar a la part superior del campus, pels carrers: <i>Sor Eulàlia d'Anzizu</i> o <i>Cavallers</i>.</p>
           </div>
         </div>
         <div class="buttons margin-bottom">
-          <a target="_blank" href="https://goo.gl/maps/6cPXChrm9GZVXimC7" class="external-link" style="margin-left: 10px">Google Maps - Barcelona</a>
-          <a target="_blank" href="https://g.page/EPS-UdL?share" class="external-link" style="margin-left: 10px; margin-right: 10px">Google Maps - Lleida</a>
+          <a target="_blank" href="https://goo.gl/maps/6cPXChrm9GZVXimC7" class="external-link">Google Maps - Barcelona</a>
         </div>
       </div>
       <div class="panel large no-margin" style="height: 100%">
