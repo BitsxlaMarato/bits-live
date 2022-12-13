48aaea1cf51f4e4a8f337a7e06f91894dd188c25
Carlota Catot Bragós <carkbra@gmail.com>
2022-12-13 23:54:06 +0100

    updated information in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,7 +17,7 @@
             <h3 class="text-centered margin-bottom" style="margin-top: 0">Links</h3>
           </div>
           <div class="buttons margin-bottom">
-            <a target="_blank" href="https://bitsxlamarato-2021.devpost.com/" class="external-link">Devpost</a>
+            <a target="_blank" href="https://bitsxlamarato-2022.devpost.com/" class="external-link">Devpost</a>
           </div>
           <p><b>Estudiants de la FIB!</b> Recordeu que per obtenir els dos crèdits és obligatori penjar el projecte a Devpost!</p>
         </div>
@@ -26,7 +26,7 @@
         <h1>Contacte</h1>
         <div class="text-centered">
           <p class="icon"><i class="fab fa-slack" aria-hidden="true"></i></p>
-          <span><a href="https://bitsxlamarato2021.slack.com/" target="_blank">Slack bitsxlaMarato 2021</a></span>
+          <span><a href="https://bitsxlamarat2022.slack.com" target="_blank">Slack bitsxlaMarato 2021</a></span>
         </div>
         <div class="text-centered">
             <p class="icon"><i class="fa fa-envelope" aria-hidden="true"></i></p>
