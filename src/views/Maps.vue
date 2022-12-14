3cd70a4abff0fe034d7b80caabff6aab3e28ca2b
Carlota Catot Bragós <carkbra@gmail.com>
2022-12-14 20:07:05 +0100

    fix image

--- src/views/Maps.vue
+++ src/views/Maps.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="panel large">
         <div class="text-centered">
-          <img src="/assets/img/mapas.jpg" alt="map">
+          <img src="/assets/img/mapas.jpg" alt="map" style="width: 80%">
         </div>
       </div>
     </div>
